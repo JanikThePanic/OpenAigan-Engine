176dc96c2e58f35d8a13f72ca7259313d15e3767
Jahangir Abdullayev <jk4abdl@gmail.com>
2024-06-27 04:33:52 -0400

    dynamic params wip 😢🔫

--- src/devmode/tabs/BotTab.py
+++ src/devmode/tabs/BotTab.py
@@ -1,12 +1,53 @@
 import tkinter as tk
 from tkinter import ttk
+import json
+
+from devmode.tabs.widgets import *
 
 
 # Return loaded settings tab frame
 def load(devGUI, engine):
     tab = ttk.Frame(devGUI.notebook, padding=devGUI.tabPadding)
 
-    # make dynamic tab that reads the json and then makes a entry for that type
+    # Currently selected bot
+    selectedBot = json.load(open(engine.userPath + "/SETTINGS.json"))["root_settings"][
+        "selected_robot"
+    ]
+    # Path to the build json
+    buildJson = json.load(open(engine.userPath + f"/build/{selectedBot}_build.json"))
+
+    # Load meta data
+    meta = buildJson["meta"]
+    # Load build parameters
+    params = buildJson["build_parameters"]
+
+    # Information up top
+    metaFrame = ttk.Frame(tab, padding=devGUI.defaultPadding)
+    metaFrame.pack(fill="x", pady=devGUI.tabPadding[1])
+
+    # Note about profile
+    note = ttk.Label(metaFrame, text=meta["note"], padding=devGUI.defaultPadding)
+    note.pack(side="left", anchor="w")
+
+    # temp save button to test things
+    saveButton = ttk.Button(metaFrame, text="Save", command=lambda: devGUI.reloadTabs())
+    saveButton.pack(side="right", anchor="e")
+
+    # Robot parameters section
+    botParams = ttk.LabelFrame(
+        tab, text=meta["profile_name"] + " Parameters", padding=devGUI.defaultPadding
+    )
+    botParams.pack(fill="x", pady=devGUI.tabPadding[1])
+
+    # Robot parameters
+    for param in params.items():
+        param = param[1]
+        # If param a...
+
+        # Float entry
+        if param["type"] == "float":
+            paramFrame = FloatEntry(botParams, param["title"], param["value"])
+            paramFrame.pack(fill="x", pady=devGUI.tabPadding[1])
 
     return tab
 
---
+++ src/devmode/tabs/widgets.py
@@ -0,0 +1,47 @@
+import tkinter as tk
+from tkinter import ttk
+
+import devmode.DevmodeGUI as gui
+
+
+# Collections of common widgets and frames and sorts for the GUI
+
+
+# Frame with test and a entry for floats
+class FloatEntry(ttk.Frame):
+    # Needs devGUI, parent, label title, initial value
+    def __init__(self, parent, title, value):
+        # Make frame
+        super().__init__(parent, padding=gui.DevmodeGUI.defaultPadding)
+
+        # Make label
+        self.label = ttk.Label(self, text=title, padding=gui.DevmodeGUI.defaultPadding)
+        self.label.pack(side="left", anchor="w")
+
+        # Make entry
+        self.entry = Lotfi(self)
+        self.entry.set(value)
+        self.entry.pack(fill="x", anchor="e")
+
+
+# Some amazing code I stole from stackoverflow
+# Minor edit, added to allow float
+class Lotfi(ttk.Entry):
+    def __init__(self, master=None, **kwargs):
+        self.var = tk.StringVar()
+        ttk.Entry.__init__(self, master, textvariable=self.var, **kwargs)
+        self.old_value = ""
+        self.var.trace("w", self.check)
+        self.get, self.set = self.var.get, self.var.set
+
+    def check(self, *args):
+        if self.get().replace(".", "", 1).isdigit():
+            # the current value is only digits; allow this
+            self.old_value = self.get()
+        else:
+            # there's non-digit characters in the input; reject this
+            self.set(self.old_value)
+
+
+if __name__ == "__main__":
+    pass
